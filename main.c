dedf56a536066280fc37309207f73db11bd3e377
J. David Ibáñez <jdavid.ibp@gmail.com>
2023-03-09 18:49:15 +0100

    Read compressed data

--- main.c
+++ main.c
@@ -37,8 +37,8 @@ int main(int argc, char* argv[])
     const char* out_fname = argv[2];
 
     // Open input file
-    FILE* finput = fopen(in_fname, "rb");
-    if (finput == NULL) {
+    blosc2_schunk *schunk_in = blosc2_schunk_open(in_fname);
+    if (schunk_in == NULL) {
         fprintf(stderr, "Input file cannot be open.\n");
         return 1;
     }
@@ -69,18 +69,19 @@ int main(int argc, char* argv[])
         .contiguous=true,
         .urlpath=(char*)out_fname
     };
-    blosc2_schunk* schunk = blosc2_schunk_new(&storage);
+    blosc2_schunk* schunk_out = blosc2_schunk_new(&storage);
 
     // Statistics
     blosc_timestamp_t t0;
     blosc_set_timestamp(&t0);
 
     // Compress
-    int8_t data[CHUNKSIZE];
-    int nchunks = get_nchunks_in_file(finput, CHUNKSIZE);
+    int chunksize = schunk_in->chunksize;
+    void *data = malloc(chunksize);
+    int nchunks = schunk_in->nchunks;
     for (int nchunk = 0; nchunk < nchunks; nchunk++) {
-        size_t size = fread(data, 1, CHUNKSIZE, finput);
-        if (blosc2_schunk_append_buffer(schunk, data, size) < 0) {
+        int size = blosc2_schunk_decompress_chunk(schunk_in, nchunk, data, chunksize);
+        if (blosc2_schunk_append_buffer(schunk_out, data, size) < 0) {
             fprintf(stderr, "Error in appending data to destination file");
             return 1;
         }
@@ -89,8 +90,8 @@ int main(int argc, char* argv[])
     // Statistics
     blosc_timestamp_t t1;
     blosc_set_timestamp(&t1);
-    int64_t nbytes = schunk->nbytes;
-    int64_t cbytes = schunk->cbytes;
+    int64_t nbytes = schunk_out->nbytes;
+    int64_t cbytes = schunk_out->cbytes;
     double ttotal = blosc_elapsed_secs(t0, t1);
     printf("Compression ratio: %.1f MB -> %.1f MB (%.1fx)\n",
             (float)nbytes / MB, (float)cbytes / MB, (1. * (float)nbytes) / (float)cbytes);
@@ -98,8 +99,8 @@ int main(int argc, char* argv[])
             ttotal, (float)nbytes / (ttotal * MB));
 
     // Free resources
-    fclose(finput);
-    blosc2_schunk_free(schunk);
+    blosc2_schunk_free(schunk_in);
+    blosc2_schunk_free(schunk_out);
     blosc2_destroy();
 
     return 0;
